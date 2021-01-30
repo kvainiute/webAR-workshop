55c55895024bf7d2371cdd32afe2c01db5c5e31d
kamilev <kamile.vainiute@gmc.stud.vu.lt>
2021-01-30 18:26:05 +0200

    turned off debugging

--- ar.js
+++ ar.js
@@ -7,116 +7,115 @@ initialize();
 animate();
 
 function initialize() {
-	scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-	let ambientLight = new THREE.AmbientLight(0xcccccc, 1.0);
-	scene.add(ambientLight);
+    let ambientLight = new THREE.AmbientLight(0xcccccc, 1.0);
+    scene.add(ambientLight);
 
-	camera = new THREE.Camera();
-	scene.add(camera);
+    camera = new THREE.Camera();
+    scene.add(camera);
 
-	renderer = new THREE.WebGLRenderer({
-		antialias: true,
-		alpha: true
-	});
-	renderer.setSize(window.innerHeight, window.innerWidth);
-	renderer.outputEncoding = THREE.sRGBEncoding;
-	document.body.appendChild(renderer.domElement);
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+    });
+    renderer.setSize(window.innerHeight, window.innerWidth);
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    document.body.appendChild(renderer.domElement);
 
-	clock = new THREE.Clock();
-	deltaTime = 0;
-	totalTime = 0;
+    clock = new THREE.Clock();
+    deltaTime = 0;
+    totalTime = 0;
 
-	////////////////////////////////////////////////////////////
-	// setup arToolkitSource
-	////////////////////////////////////////////////////////////
-	const width = window.innerWidth;
-	const height = window.innerHeight;
-	arToolkitSource = new THREEx.ArToolkitSource({
-		// to read from the webcam
-		sourceType: 'webcam',
+    ////////////////////////////////////////////////////////////
+    // setup arToolkitSource
+    ////////////////////////////////////////////////////////////
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    arToolkitSource = new THREEx.ArToolkitSource({
+        // to read from the webcam
+        sourceType: 'webcam',
 
-	})
+    })
 
-	arToolkitSource.init(function onReady() {
-		onResize()
-		setTimeout(onResize, 1000);
-	})
+    arToolkitSource.init(function onReady() {
+        onResize()
+        setTimeout(onResize, 1000);
+    })
 
-	// handle resize
-	window.addEventListener('resize', function () {
-		onResize()
-	})
+    // handle resize
+    window.addEventListener('resize', function() {
+        onResize()
+    })
 
-	function onResize() {
-		arToolkitSource.onResizeElement()
-		arToolkitSource.copyElementSizeTo(renderer.domElement)
-		if (arToolkitContext.arController !== null) {
-			arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
-		}
-	}
-	////////////////////////////////////////////////////////////
-	// setup arToolkitContext
-	////////////////////////////////////////////////////////////	
+    function onResize() {
+        arToolkitSource.onResizeElement()
+        arToolkitSource.copyElementSizeTo(renderer.domElement)
+        if (arToolkitContext.arController !== null) {
+            arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
+        }
+    }
+    ////////////////////////////////////////////////////////////
+    // setup arToolkitContext
+    ////////////////////////////////////////////////////////////	
 
-	// create atToolkitContext
-	arToolkitContext = new THREEx.ArToolkitContext({
-		cameraParametersUrl: 'data/camera_para.dat',
-		detectionMode: 'mono',
-		debug: true,
-	});
+    // create atToolkitContext
+    arToolkitContext = new THREEx.ArToolkitContext({
+        cameraParametersUrl: 'data/camera_para.dat',
+        detectionMode: 'mono',
+    });
 
-	// copy projection matrix to camera when initialization complete
-	arToolkitContext.init(function onCompleted() {
-		camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
-	});
+    // copy projection matrix to camera when initialization complete
+    arToolkitContext.init(function onCompleted() {
+        camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
+    });
 
-	////////////////////////////////////////////////////////////
-	// setup markerRoots
-	////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////
+    // setup markerRoots
+    ////////////////////////////////////////////////////////////
 
-	// build markerControls
-	markerRoot = new THREE.Group();
-	scene.add(markerRoot);
-	let markerControls = new THREEx.ArMarkerControls(arToolkitContext, markerRoot, {
-		type: 'pattern',
-		patternUrl: "data/marker.patt",
-	})
+    // build markerControls
+    markerRoot = new THREE.Group();
+    scene.add(markerRoot);
+    let markerControls = new THREEx.ArMarkerControls(arToolkitContext, markerRoot, {
+        type: 'pattern',
+        patternUrl: "data/marker.patt",
+    })
 
 
-	let geometry = new THREE.IcosahedronGeometry(1, 5);
-	geometry.translate(0, 1, 1)
-	let material = new THREE.MeshBasicMaterial({
-		color: 0x6281ef,
-		opacity: 0.3
-	});
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.rotation.x = -Math.PI / 2;
-	markerRoot.add(mesh);
+    let geometry = new THREE.IcosahedronGeometry(1, 5);
+    geometry.translate(0, 1, 1)
+    let material = new THREE.MeshBasicMaterial({
+        color: 0x6281ef,
+        opacity: 0.3
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = -Math.PI / 2;
+    markerRoot.add(mesh);
 
-	//	let loader = new THREE.GLTFLoader();
-	//	loader.load('./scene.glb', function (gltf) {
-	//		gltf.scene.scale.set(0.004, 0.005, 0.005);
-	//		//		gltf.scene.position.x += 1
-	//		mixer = new THREE.AnimationMixer(gltf.scene);
-	//		gltf.animations.forEach((clip) => {
-	//			mixer.clipAction(clip).play();
-	//
-	//		});
-	//		markerRoot.add(gltf.scene);
-	//	});
+    //	let loader = new THREE.GLTFLoader();
+    //	loader.load('./scene.glb', function (gltf) {
+    //		gltf.scene.scale.set(0.004, 0.005, 0.005);
+    //		//		gltf.scene.position.x += 1
+    //		mixer = new THREE.AnimationMixer(gltf.scene);
+    //		gltf.animations.forEach((clip) => {
+    //			mixer.clipAction(clip).play();
+    //
+    //		});
+    //		markerRoot.add(gltf.scene);
+    //	});
 }
 
 function animate() {
-	requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-	deltaTime = clock.getDelta();
-	totalTime += deltaTime;
-	if (mixer) mixer.update(deltaTime);
+    deltaTime = clock.getDelta();
+    totalTime += deltaTime;
+    if (mixer) mixer.update(deltaTime);
 
-	// update artoolkit on every frame
-	if (arToolkitSource.ready !== false)
-		arToolkitContext.update(arToolkitSource.domElement);
+    // update artoolkit on every frame
+    if (arToolkitSource.ready !== false)
+        arToolkitContext.update(arToolkitSource.domElement);
 
-	renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
