442d4dc9012f29c982c29f2d53916f4e54f61100
kamilev <kamile.vainiute@gmc.stud.vu.lt>
2021-01-30 18:18:13 +0200

    webcam screen size fix

--- ar.js
+++ ar.js
@@ -12,15 +12,14 @@ function initialize() {
 	let ambientLight = new THREE.AmbientLight(0xcccccc, 1.0);
 	scene.add(ambientLight);
 
-	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+	camera = new THREE.Camera();
 	scene.add(camera);
 
 	renderer = new THREE.WebGLRenderer({
 		antialias: true,
-		preserveDrawingBuffer: true,
 		alpha: true
 	});
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerHeight, window.innerWidth);
 	renderer.outputEncoding = THREE.sRGBEncoding;
 	document.body.appendChild(renderer.domElement);
 
@@ -31,28 +30,31 @@ function initialize() {
 	////////////////////////////////////////////////////////////
 	// setup arToolkitSource
 	////////////////////////////////////////////////////////////
-
+	const width = window.innerWidth;
+	const height = window.innerHeight;
 	arToolkitSource = new THREEx.ArToolkitSource({
+		// to read from the webcam
 		sourceType: 'webcam',
-	});
 
-	function onResize() {
-		arToolkitSource.onResize()
-		arToolkitSource.copySizeTo(renderer.domElement)
-		if (arToolkitContext.arController !== null) {
-			arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)
-		}
-	}
+	})
 
 	arToolkitSource.init(function onReady() {
 		onResize()
-	});
+		setTimeout(onResize, 1000);
+	})
 
-	// handle resize event
+	// handle resize
 	window.addEventListener('resize', function () {
 		onResize()
-	});
+	})
 
+	function onResize() {
+		arToolkitSource.onResizeElement()
+		arToolkitSource.copyElementSizeTo(renderer.domElement)
+		if (arToolkitContext.arController !== null) {
+			arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
+		}
+	}
 	////////////////////////////////////////////////////////////
 	// setup arToolkitContext
 	////////////////////////////////////////////////////////////	
@@ -60,7 +62,8 @@ function initialize() {
 	// create atToolkitContext
 	arToolkitContext = new THREEx.ArToolkitContext({
 		cameraParametersUrl: 'data/camera_para.dat',
-		detectionMode: 'mono'
+		detectionMode: 'mono',
+		debug: true,
 	});
 
 	// copy projection matrix to camera when initialization complete
@@ -80,27 +83,28 @@ function initialize() {
 		patternUrl: "data/marker.patt",
 	})
 
-	//	let geometry = new THREE.IcosahedronGeometry(1, 5);
-	//	geometry.translate(1, 1, 1)
-	//	let material = new THREE.MeshBasicMaterial({
-	//		color: 0x6281ef,
-	//		opacity: 0.3
-	//	});
-	//	mesh = new THREE.Mesh(geometry, material);
-	//	mesh.rotation.x = -Math.PI / 2;
-	//	markerRoot.add(mesh);
 
-	let loader = new THREE.GLTFLoader();
-	loader.load('./scene.glb', function (gltf) {
-		gltf.scene.scale.set(0.002, 0.005, 0.005);
-		gltf.scene.position.x += 1
-		mixer = new THREE.AnimationMixer(gltf.scene);
-		gltf.animations.forEach((clip) => {
-			mixer.clipAction(clip).play();
-
-		});
-		markerRoot.add(gltf.scene);
+	let geometry = new THREE.IcosahedronGeometry(1, 5);
+	geometry.translate(0, 1, 1)
+	let material = new THREE.MeshBasicMaterial({
+		color: 0x6281ef,
+		opacity: 0.3
 	});
+	mesh = new THREE.Mesh(geometry, material);
+	mesh.rotation.x = -Math.PI / 2;
+	markerRoot.add(mesh);
+
+	//	let loader = new THREE.GLTFLoader();
+	//	loader.load('./scene.glb', function (gltf) {
+	//		gltf.scene.scale.set(0.004, 0.005, 0.005);
+	//		//		gltf.scene.position.x += 1
+	//		mixer = new THREE.AnimationMixer(gltf.scene);
+	//		gltf.animations.forEach((clip) => {
+	//			mixer.clipAction(clip).play();
+	//
+	//		});
+	//		markerRoot.add(gltf.scene);
+	//	});
 }
 
 function animate() {
